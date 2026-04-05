cb7422143cdcbe35ec15dae229905379ea13b9a5
Fatehveer Chahal <fveer10@gmail.com>
2026-04-05 18:36:11 -0300

    Add files via upload
    Updated my website using flex and grid system

--- index_1.js
+++ index_1.js
@@ -11,15 +11,16 @@ if(document.getElementById("Alert")){
 function greetingDisplay(){
 	let hour = new Date().getHours();
 	let greeting = document.getElementById("greets");
-
-	if (hour < 12){
-		greeting.innerText = "Good Morning! I'm Fatehveer Chahal.";
-	}
-	else if (hour < 18){
-		greeting.innerText = "Good Afternoon! I'm Fatehveer Chahal.";
-	}
-	else {
-		greeting.innerText = "Good Evening! I'm Fatehveer Chahal.";
+	if (greeting){
+		if (hour < 12){
+			greeting.innerText = "Good Morning! I'm Fatehveer Chahal.";
+		}
+		else if (hour < 18){
+			greeting.innerText = "Good Afternoon! I'm Fatehveer Chahal.";
+		}
+		else {
+			greeting.innerText = "Good Evening! I'm Fatehveer Chahal.";
+		}
 	}
 }
 greetingDisplay();
@@ -41,7 +42,7 @@ timeDisplay();
 
 function changeColor(theme){
 	document.body.style.backgroundImage = "none";
-	const navAndFooter = document.querySelectorAll('nav ul, footer');
+	const navAndFooter = document.querySelectorAll('nav ul, footer, #hero');
 	if (theme == "dark"){
 		document.body.style.backgroundColor = "black";
 		document.body.style.color = "white";
@@ -60,19 +61,21 @@ function changeColor(theme){
 	else{
 		document.body.style.backgroundImage = "url(backgroundImage.jpg)";
 		document.body.style.backgroundSize = "cover";
-		document.body.style.backgroundArrachment = "fixed";
+		document.body.style.backgroundAttachment = "fixed";
 		document.body.style.color = "black";
 		navAndFooter.forEach(element => {
-			element.style.backgroundColor = "steelBlue";
+			element.style.backgroundColor = "#1B2A4A";
 		});
 	}
 }
 
+//mouse hover action
+
 function mouseOver(){
 	let box = document.getElementById("interactive");
 	box.innerHTML = "<b> Cybersecurity Focus: </b> Penetration Testing, Java Logic, and Network Security.";
-	box.style.backgroundColor = "Red";
-	box.style.color = "Blue";	
+	box.style.backgroundColor = "Yellow";
+	box.style.color = "grey";	
 	box.style.border = "2px solid black";
 }
 
@@ -80,7 +83,7 @@ function mouseOut(){
 	let box = document.getElementById("interactive");
 	box.innerHTML = "Hover your mouse over here";
 	box.style.backgroundColor = "transparent";
-	box.style.color = "red";
-	box.style.border = "2px dashed red";
+	box.style.color = "black";
+	box.style.border = "none";
 }
 			
